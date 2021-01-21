ae569d7974c2c3a34c6e9a6f3302076ae75853da
rrkumawatcst <rrkumawatcst@gmail.com>
2021-01-21 14:00:54 +0530

    First Update From VS2015

---
+++ SqlDBClass/SqlDBInstance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace SqlDBClass
+{
+    public class SqlDBInstance
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+        Hashtable ht;
+
+    }
+}
