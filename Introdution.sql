91a8cf77599d6e8312c17e8760cd7d675940b3e2
rrkumawatcst <77613617+rrkumawatcst@users.noreply.github.com>
2021-01-23 16:06:22 +0530

    upload first Introdution.sql file

---
+++ Introdution.sql
@@ -0,0 +1,24 @@
+/*
+	this is introduciton file for testing and data access from SQL Server
+	-------------------------------------------------------------------------
+	Created Date : 23-01-2021 : 16:01
+	-------------------------------------------------------------------------
+*/
+CREATE DATABASE GLPDB
+USE GLPDB
+
+
+CREATE TABLE TEST_TABLE
+(
+	S_ID INT NOT NULL,
+	STATE_NAME VARCHAR(100)
+)
+
+insert into TEST_TABLE(S_ID,STATE_NAME)VALUES(1,'MAHARASHTRA')
+insert into TEST_TABLE(S_ID,STATE_NAME)VALUES(2,'GUJARAT')
+insert into TEST_TABLE(S_ID,STATE_NAME)VALUES(3,'MADHYA PRADESH')
+insert into TEST_TABLE(S_ID,STATE_NAME)VALUES(4,'RAJASTAN')
+insert into TEST_TABLE(S_ID,STATE_NAME)VALUES(5,'HIMACHAL PRADESH')
+
+
+SELECT S_ID,STATE_NAME FROM TEST_TABLE
